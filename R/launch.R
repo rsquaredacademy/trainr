24d57883e17260845d7e7ca7239d6351d93912db
rsquaredin <rsquaredin@gmail.com>
2018-05-21 16:13:30 +0530

    add tutorial description

--- R/launch.R
+++ R/launch.R
@@ -1,20 +1,184 @@
+#' @title Launch tutorials
+#' @description Launch interactive tutorials
+#' @export
+#'
+tr_launch_tutorial <- function() {
+  
+  tutorial <- menu(c("Importing Data into R - Part 1", 
+                     "Importing Data into R - Part 2",
+                     "Introduction to tibbles", 
+                     "Readable Code with Pipes",
+                     "Data Wrangling with dplyr - Part 1",
+                     "Data Wrangling with dplyr - Part 2",
+                     "Data Wrangling with dplyr - Part 3",
+                     "Hacking Strings with stringr",
+                     "Manipulating Date with lubridate",
+                     "Categorical Data Analysis with forcates"))
+  
+  if (tutorial == 1) {
+    tr_import_part_1()
+  } else if (tutorial == 2) {
+    tr_import_part_1()
+  } else if (tutorial == 3) {
+    tr_intro_tibbles()
+  } else if (tutorial == 4) {
+    tr_intro_pipes()
+  } else if (tutorial == 5) {
+    tr_dplyr_part_1()
+  } else if (tutorial == 6) {
+    tr_dplyr_part_2()
+  } else if (tutorial == 7) {
+    tr_dplyr_part_3()
+  } else if (tutorial == 8) {
+    tr_string_manipulation()
+  } else if (tutorial == 9) {
+    tr_date_manipulation()
+  } else if (tutorial == 10) {
+    tr_categorical_analysis()
+  } 
+  
+}
+
 #' @importFrom learnr run_tutorial
 #' @importFrom pingr ping
 #' @title Learn dplyr
 #' @description dplyr tutorial
 #' @export
 #'
-learn_dplyr <- function() {
+tr_dplyr_part_1 <- function() {
+  
   if (is.na(ping('www.google.com', count = 1))) {
     stop('Please ensure that you are connected to the internet.', call. = FALSE)
   }
-  learnr::run_tutorial(name = 'dplyr-part-1', package = 'trainr')
+
+  run_tutorial(name = 'data-wrangling-with-dplyr-part-1', package = 'trainr')
+
 }
 
-#' @title Learn Matrix
-#' @description Matrix tutorial
+#' @title Learn dplyr
+#' @description dplyr tutorial
 #' @export
 #'
-learn_matrix <- function() {
-  learnr::run_tutorial(name = 'matrix', package = 'trainr')
+tr_dplyr_part_2 <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'data-wrangling-with-dplyr-part-2', package = 'trainr')
+
 }
+
+
+#' @title Learn dplyr
+#' @description dplyr tutorial
+#' @export
+#'
+tr_dplyr_part_3 <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'data-wrangling-with-dplyr-part-3', package = 'trainr')
+
+}
+
+#' @title Import Data
+#' @description Read data from flat and delimited files.
+#' @export
+#'
+tr_import_part_1 <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'import-data-in-r-part-1', package = 'trainr')
+
+}
+
+#' @title Import Data
+#' @description Read data from excel spreadsheet.
+#' @export
+#'
+tr_import_part_2 <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'import-data-in-r-part-2', package = 'trainr')
+
+}
+
+#' @title tibbles
+#' @description Introduction to tibbles.
+#' @export
+#'
+tr_intro_tibbles <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'introduction-to-tibbles', package = 'trainr')
+
+}
+
+#' @title Pipes
+#' @description Readable code with pipes.
+#' @export
+#'
+tr_intro_pipes <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'readable-code-with-pipes', package = 'trainr')
+
+}
+
+#' @title String manipulation
+#' @description Hacking strings with stringr.
+#' @export
+#'
+tr_string_manipulation <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'hacking-strings-with-stringr', package = 'trainr')
+
+}
+
+#' @title Date manipulation
+#' @description Working with date and time using lubridate.
+#' @export
+#'
+tr_date_manipulation <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'work-with-date-and-time-in-R', package = 'trainr')
+
+}
+
+#' @title Categorical data analysis
+#' @description Launch tutorial to learn working with categorical data.
+#' @export
+#'
+tr_categorical_analysis <- function() {
+  
+  if (is.na(ping('www.google.com', count = 1))) {
+    stop('Please ensure that you are connected to the internet.', call. = FALSE)
+  }
+
+  run_tutorial(name = 'working-with-categorical-data', package = 'trainr')
+
+}
+
--- R/tutorials.R
+++ R/tutorials.R
@@ -1,20 +1,195 @@
 #' @importFrom learnr run_tutorial
-#' @importFrom cli rule
 #' @importFrom clisymbols symbol
 #' @importFrom crayon red green
 #' @title Display Tutorials
 #' @description Show all tutorials
 #' @export
 
-learn_tutorials_list <- function() {
-  # locate all the tutorials that exist
-  validExamples <- list.files(system.file("tutorials", package = "trainr"))
-
-  validExamplesMsg <-
-    paste0(
-      rule(center = red(" List of Tutorials "), width = 60), "\n",
-      (paste(green(symbol$tick), validExamples, collapse = "\n")))
-
-  cat(validExamplesMsg)
+tr_tutorials_list <- function() {
+  
+  tutorial <- menu(c("Importing Data into R - Part 1", 
+                     "Importing Data into R - Part 2",
+                     "Data Wrangling with dplyr - Part 1",
+                     "Data Wrangling with dplyr - Part 2",
+                     "Data Wrangling with dplyr - Part 3",
+                     "Hacking Strings with stringr",
+                     "Manipulating Date with lubridate",
+                     "Categorical Data Analysis with forcats",
+                     "Introduction to tibbles", 
+                     "Readable Code with Pipes"))
+  
+  if (tutorial == 1) {
+    cat(crayon::bold$blue$underline(" Import Data into R - Part 1 "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read data from flat/delimited files"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "skip lines if text if present before data in the file"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "specify whether column names are present/absent"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect column types in the data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "specify column types before reading the data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read specific set of columns from the data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "and skip columns while reading data")
+    )
+  } else if (tutorial == 2) {
+    cat(crayon::bold$blue$underline(" Import Data into R - Part 2 "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "list sheets in an excel file"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read data from an excel sheet"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read data from specific cells in a sheet"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read specific rows of data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read specific columns of data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "read data from SAS, STATA and SPSS files")
+    )
+  } else if (tutorial == 3) {
+    cat(crayon::bold$blue$underline(" Data Wrangling with dplyr - Part 1 "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "filter rows"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "select columns"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "sort data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "create grouped summaries"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "create new variables")
+    )
+  } else if (tutorial == 4) {
+    cat(crayon::bold$blue$underline(" Data Wrangling with dplyr - Part 2 "),  
+    "\n\n In this tutorial, you will learn to join tables using:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "inner join"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "left join"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "right join"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "semi join"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "anti join"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "full join")
+    )
+  } else if (tutorial == 5) {
+    cat(crayon::bold$blue$underline(" Data Wrangling with dplyr - Part 3 "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "extract unique rows"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "rename columns"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "sample data"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "extract columns"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "slice rows"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "sort rows"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "generate counts"), "\n"
+    )
+  } else if (tutorial == 6) {
+    cat(crayon::bold$blue$underline(" Hacking Strings with stringr "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "split strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "concatenate strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "compute length of strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "locate patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "replace patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "change case of a string"), "\n"
+    )
+  } else if (tutorial == 7) {
+    cat(crayon::bold$blue$underline(" Manipulating Date & Time with lubridate "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "split strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "concatenate strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "compute length of strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "locate patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "replace patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "change case of a string"), "\n"
+    )
+  } else if (tutorial == 8) {
+    cat(crayon::bold$blue$underline(" Categorical Data Analysis with forcats "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "split strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "concatenate strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "compute length of strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "locate patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "replace patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "change case of a string"), "\n"
+    )
+  } else if (tutorial == 9) {
+    cat(crayon::bold$blue$underline(" Introduction to tibbles "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "split strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "concatenate strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "compute length of strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "locate patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "replace patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "change case of a string"), "\n"
+    )
+  } else if (tutorial == 10) {
+    cat(crayon::bold$blue$underline(" Readable Code with Pipes "),  
+    "\n\n In this tutorial, you will learn to:", "\n\n", 
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "detect patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "split strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "concatenate strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "compute length of strings"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "locate patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "replace patterns in a string"), "\n",
+    paste(crayon::blue(clisymbols::symbol$tick), 
+          "change case of a string"), "\n"
+    )
+  }
 
 }
